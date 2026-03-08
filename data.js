9d80953a65dd2e9ba235b30f47d0b88ab0b24628
Brokejs <igaza8934@gmail.com>
2026-03-08 21:51:44 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,50 @@
+const animeData = [
+  {
+    id: 1,
+    title: "Naruto",
+    category: "Action",
+    poster: "images/naruto.jpeg",
+    episodes: [
+      {
+        name: "Episode 1",
+        video: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8"
+      },
+      {
+        name: "Episode 2",
+        video: "https://demo.unified-streaming.com/k8s/features/stable/video/tears-of-steel/tears-of-steel.ism/.m3u8"
+      }
+    ]
+  },
+  {
+    id: 2,
+    title: "One Piece",
+    category: "Adventure",
+    poster: "images/one_piece.jpg",
+    episodes: [
+      {
+        name: "Episode 1",
+        video: "https://www.youtube.com/embed/447mQ4PR9wo"
+      }
+    ]
+  },
+  {
+    id: 3,
+    title: "dragon balls",
+    category: "Action",
+    poster: "images/dragon.jpg",
+  episodes: [
+  {
+    name: "Test Stream 1",
+    video: "https://youtube.com/embed/XpoZVGtMHcA"
+  },
+  {
+    name: "Tears of Steel",
+    video: "https://youtube.com/embed/8Ry9f5x3sww"
+  },
+  {
+    name: "Apple Sample",
+    video: "https://youtube.com/embed/AwhUDZT14jY"
+  }
+ ]
+}
+];
